a8c5a044bf1d0ffd0697d866a58923cd72c47834
Guido <guidobedman@gmail.com>
2022-03-29 17:10:30 -0300

    Versión previa a many to one o one to many.

--- Portfolio/Portfolio/src/main/java/com/PortfolioObedmanGuido/Portfolio/repository/Experiencia_LaboralRepository.java
+++ Portfolio/Portfolio/src/main/java/com/PortfolioObedmanGuido/Portfolio/repository/Experiencia_LaboralRepository.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+@Repository
 public interface Experiencia_LaboralRepository extends JpaRepository<Experiencia_Laboral, Long>{
     Optional<Experiencia_Laboral> findByPosition(String po);
     boolean existsByPosition(String po);
